4a1aaec936d19ed9f29dec88b136a6daf1b42f70
jimmy feriawan <jimyferi@gmail.com>
2023-10-14 20:41:11 +0700

    update: dump DATABASE template

--- blog.sql
+++ blog.sql
@@ -1,71 +1,52 @@
-CREATE DATABASE  IF NOT EXISTS `blog_beta` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `blog_beta`;
--- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: blog
--- ------------------------------------------------------
--- Server version	8.0.14
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `category`
---
+-- Dumping database structure for blog
+CREATE DATABASE IF NOT EXISTS `blog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `blog`;
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `category` (
+
+-- Dumping structure for table blog.category
+CREATE TABLE IF NOT EXISTS `category` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `parentId` bigint(20) DEFAULT NULL,
-  `title` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `metaTitle` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `slug` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `title` varchar(75) NOT NULL,
+  `metaTitle` varchar(100) DEFAULT NULL,
+  `slug` varchar(100) NOT NULL,
+  `content` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_category_parent` (`parentId`),
   CONSTRAINT `fk_category_parent` FOREIGN KEY (`parentId`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `category`
---
-
-LOCK TABLES `category` WRITE;
+-- Dumping data for table blog.category: ~0 rows (approximately)
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `post`
---
 
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post` (
+-- Dumping structure for table blog.post
+CREATE TABLE IF NOT EXISTS `post` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `authorId` bigint(20) NOT NULL,
   `parentId` bigint(20) DEFAULT NULL,
-  `title` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `metaTitle` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `slug` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `summary` tinytext COLLATE utf8mb4_unicode_ci,
-  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `title` varchar(75) NOT NULL,
+  `metaTitle` varchar(100) DEFAULT NULL,
+  `slug` varchar(100) NOT NULL,
+  `summary` tinytext DEFAULT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT 0,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `publishedAt` datetime DEFAULT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `content` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uq_slug` (`slug`),
   KEY `idx_post_user` (`authorId`),
@@ -73,113 +54,69 @@ CREATE TABLE `post` (
   CONSTRAINT `fk_post_parent` FOREIGN KEY (`parentId`) REFERENCES `post` (`id`),
   CONSTRAINT `fk_post_user` FOREIGN KEY (`authorId`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `post`
---
-
-LOCK TABLES `post` WRITE;
+-- Dumping data for table blog.post: ~0 rows (approximately)
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `post_category`
---
 
-DROP TABLE IF EXISTS `post_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post_category` (
+-- Dumping structure for table blog.post_category
+CREATE TABLE IF NOT EXISTS `post_category` (
   `postId` bigint(20) NOT NULL,
   `categoryId` bigint(20) NOT NULL,
   PRIMARY KEY (`postId`,`categoryId`),
   KEY `idx_pc_category` (`categoryId`),
-  KEY `idx_pc_post` (`postId`) /*!80000 INVISIBLE */,
+  KEY `idx_pc_post` (`postId`),
   CONSTRAINT `fk_pc_category` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`),
   CONSTRAINT `fk_pc_post` FOREIGN KEY (`postId`) REFERENCES `post` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `post_category`
---
-
-LOCK TABLES `post_category` WRITE;
+-- Dumping data for table blog.post_category: ~0 rows (approximately)
 /*!40000 ALTER TABLE `post_category` DISABLE KEYS */;
 /*!40000 ALTER TABLE `post_category` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `post_comment`
---
 
-DROP TABLE IF EXISTS `post_comment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post_comment` (
+-- Dumping structure for table blog.post_comment
+CREATE TABLE IF NOT EXISTS `post_comment` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `postId` bigint(20) NOT NULL,
   `parentId` bigint(20) DEFAULT NULL,
-  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `title` varchar(100) NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT 0,
   `createdAt` datetime NOT NULL,
   `publishedAt` datetime DEFAULT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `content` text DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_comment_post` (`postId`) /*!80000 INVISIBLE */,
+  KEY `idx_comment_post` (`postId`),
   KEY `idx_comment_parent` (`parentId`),
   CONSTRAINT `fk_comment_parent` FOREIGN KEY (`parentId`) REFERENCES `post_comment` (`id`),
   CONSTRAINT `fk_comment_post` FOREIGN KEY (`postId`) REFERENCES `post` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `post_comment`
---
-
-LOCK TABLES `post_comment` WRITE;
+-- Dumping data for table blog.post_comment: ~0 rows (approximately)
 /*!40000 ALTER TABLE `post_comment` DISABLE KEYS */;
 /*!40000 ALTER TABLE `post_comment` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `post_meta`
---
 
-DROP TABLE IF EXISTS `post_meta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post_meta` (
+-- Dumping structure for table blog.post_meta
+CREATE TABLE IF NOT EXISTS `post_meta` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `postId` bigint(20) NOT NULL,
-  `key` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `key` varchar(50) NOT NULL,
+  `content` text DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uq_post_meta` (`postId`,`key`) /*!80000 INVISIBLE */,
+  UNIQUE KEY `uq_post_meta` (`postId`,`key`),
   KEY `idx_meta_post` (`postId`),
   CONSTRAINT `fk_meta_post` FOREIGN KEY (`postId`) REFERENCES `post` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `post_meta`
---
-
-LOCK TABLES `post_meta` WRITE;
+-- Dumping data for table blog.post_meta: ~0 rows (approximately)
 /*!40000 ALTER TABLE `post_meta` DISABLE KEYS */;
 /*!40000 ALTER TABLE `post_meta` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `post_tag`
---
 
-DROP TABLE IF EXISTS `post_tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `post_tag` (
+-- Dumping structure for table blog.post_tag
+CREATE TABLE IF NOT EXISTS `post_tag` (
   `postId` bigint(20) NOT NULL,
   `tagId` bigint(20) NOT NULL,
   PRIMARY KEY (`postId`,`tagId`),
@@ -188,84 +125,53 @@ CREATE TABLE `post_tag` (
   CONSTRAINT `fk_pt_post` FOREIGN KEY (`postId`) REFERENCES `post` (`id`),
   CONSTRAINT `fk_pt_tag` FOREIGN KEY (`tagId`) REFERENCES `tag` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `post_tag`
---
-
-LOCK TABLES `post_tag` WRITE;
+-- Dumping data for table blog.post_tag: ~0 rows (approximately)
 /*!40000 ALTER TABLE `post_tag` DISABLE KEYS */;
 /*!40000 ALTER TABLE `post_tag` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `tag`
---
 
-DROP TABLE IF EXISTS `tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `tag` (
+-- Dumping structure for table blog.tag
+CREATE TABLE IF NOT EXISTS `tag` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` varchar(75) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `metaTitle` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `slug` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `title` varchar(75) NOT NULL,
+  `metaTitle` varchar(100) DEFAULT NULL,
+  `slug` varchar(100) NOT NULL,
+  `content` text DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `tag`
---
-
-LOCK TABLES `tag` WRITE;
+-- Dumping data for table blog.tag: ~0 rows (approximately)
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `user`
---
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user` (
+-- Dumping structure for table blog.user
+CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `middleName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `mobile` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `passwordHash` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `middleName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `passwordHash` varchar(32) NOT NULL,
   `registeredAt` datetime NOT NULL,
   `lastLogin` datetime DEFAULT NULL,
-  `intro` tinytext COLLATE utf8mb4_unicode_ci,
-  `profile` text COLLATE utf8mb4_unicode_ci,
+  `intro` tinytext DEFAULT NULL,
+  `profile` text DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `emailConfirmed` varchar(1) DEFAULT 'N',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uq_mobile` (`mobile`),
-  UNIQUE KEY `uq_emai` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE KEY `uq_email` (`email`),
+  UNIQUE KEY `uq_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
+-- Dumping data for table blog.user: ~1 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `firstName`, `middleName`, `lastName`, `mobile`, `email`, `passwordHash`, `registeredAt`, `lastLogin`, `intro`, `profile`, `username`, `emailConfirmed`) VALUES
+	(1, 'Jimmy', 'Feriawan', '', '087784517748', 'feriawanjimmy@mail.com', '$2b$10$VehOVJirUqtMcdxBZSp44.gNi', '2023-10-13 09:30:08', NULL, NULL, NULL, 'jimmy', 'N');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-11-29 10:09:20
