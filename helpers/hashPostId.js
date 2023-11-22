144cd9f115015b286fefd0dd68823fdd43e5ff6a
jimmy feriawan <jimyferi@gmail.com>
2023-11-22 13:20:25 +0700

    registrasi : generate activation link function

--- helpers/hashPostId.js
+++ helpers/hashPostId.js
@@ -8,5 +8,10 @@ const generatePostSlugAdd = new Sqids({
   alphabet: "jUmC0c1iT2EvlMHnPZth5GwQbW8fSYg7NOBLuxodR6eKDF4zq3askXIyp9VrJA",
   minLength: 8,
 });
+// should be generated from next auto_increment : SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = "blog" AND TABLE_NAME = "user";
+const generateActivation = new Sqids({
+  alphabet: "BJ0bpMh7jaRIQgKZwPodx8zVN56svEiAWUCDuScOt2XYnFrk9qTeGy1lmL3H4f",
+  minLength: 125,
+});
 
-module.exports = { generatePostId, generatePostSlugAdd };
+module.exports = { generatePostId, generatePostSlugAdd, generateActivation };
