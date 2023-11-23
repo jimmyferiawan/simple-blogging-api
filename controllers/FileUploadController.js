ba9f4353bc0bae53f01ae004e42aedaf1592156f
jimmy feriawan <jimyferi@gmail.com>
2023-11-23 10:19:37 +0700

    enh: max file size limit

--- controllers/FileUploadController.js
+++ controllers/FileUploadController.js
@@ -1,18 +1,30 @@
+const multer = require("multer");
+
 function imageProfileUpload(uploadFileService) {
   return async (req, res) => {
     uploadFileService(req, res, function (err) {
-      console.log("file => ", req.file);
+      // console.log("file => ", req.file);
       if (err) {
-        console.log("error => ", err);
-        res.status(500).send({
-          error: true,
-          message: "Terjadi kesalahan, silahkan coba beberapa saat lagi",
-        });
+        let body = {error: true, message: "Terjadi kesalahan, silahkan coba beberapa saat lagi"}
+        if (err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading.
+          console.log("error => ", err.code);
+          switch (err.code) {
+            case "LIMIT_FILE_SIZE":
+              body.message = "File size should be under 2MB"
+              break;
+          
+            default:
+              break;
+          }
+        }
+        // console.log("error => ", err);
+        res.status(500).send(body);
       } else {
         res.status(200).send({
           error: false,
           message: "Ok",
-          data: { fileName: req.file.filename },
+          data: { fileName: req.file.filename || ""},
         });
       }
     });
--- services/FileUploadService.js
+++ services/FileUploadService.js
@@ -1,19 +1,21 @@
 const { generateAdditionalFileName } = require("../helpers/hashPostId");
-const multer = require("multer")
+const multer = require("multer");
 // import { generateAdditionalFileName } from '../helpers/hashPostId'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(`destination : ${process.env.WORKING_DIR}\\assets\\images`)
+    // console.log(`destination : ${process.env.WORKING_DIR}\\assets\\images`);
+    // console.log(`filesize : ${file}`);
     cb(null, `${process.env.WORKING_DIR}\\assets\\images`);
   },
   filename: (req, file, cb) => {
-    console.log(`filename : ${file.filename} | mimetype : ${file.mimetype}`)
+    // console.log(`filename : ${file.filename} | mimetype : ${file.mimetype}`);
     cb(null, generateAdditionalFileName.encode([Date.now()]) + "_" + file.originalname);
-  }
+  },
 });
 
-const upload = multer({ storage: storage });
-const uploadFile = upload.single("file")
+const maxSize = 2 * 1024 * 1024;
+const upload = multer({ storage: storage, limits: { fileSize: maxSize } });
+const uploadFile = upload.single("file");
 
 module.exports = uploadFile;
